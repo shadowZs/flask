cd2cd4b4ce19b83434d5e835f06f5f7b18c271c5
张山 <zhangshan@wtoip.com>
2019-09-16 18:22:13 +0800

    first push

--- app.py
+++ app.py
@@ -1,28 +1,84 @@
 from flask_cors import CORS
-from flask import Flask, Response, jsonify, request
-from models import insert_user, create_user_table
+from flask import Flask, Response, jsonify, request, url_for, redirect, session
+from werkzeug import secure_filename   # 用来对文件名进行安全监测
+import json
+from models import DBManager
+import os
+
+# static_url_path  static_folder 配置静态资源文件夹
+app = Flask(__name__, static_url_path='/static')
+app.config['base_host'] = 'http://localhost:5000'
+
+DBManager().create_user_table()
+DBManager().create_article_list()
+
+# form 表单提交可以通过 request.form.get() / form[] 来获取参数
+# post 非表单提交可以通过 request.get_data 来获取,
+# json.loads(s)  json解码 （json.load 是用来处理文件） json.dumps() json 编码
 
 
-app = Flask(__name__)
+# session 登录验证
+# def wrapper(func):
 
-create_user_table()
 
+# 登录
 @app.route("/login", methods=['POST'])
 def login():
-	return jsonify({'code': 1, 'message': '登录成功', 'data':'1111'})
+	data = request.get_data()
+	res = json.loads(data)
+	print('data:', res)
+	mobile = res['mobile']
+	password = res['password']
+	result = DBManager().get_user_by_mobile(mobile)
+	print('result:', result)
+	if result['password'] == password:
+		return jsonify({'code': 1, 'message': '登录成功', 'data': result})
+	else:
+		return jsonify({'code': 0, 'message': '账号密码错误', 'data': '账号密码错误'})
 
-
-@app.route('/register', methods= ['POST'])
+# 注册
+@app.route('/register', methods=['POST'])
 def register():
-	mobile = request.form.get['mobile']
-	password = request.form.get['password']
-	nick_name = request.form.get['nickName']
+	data = request.get_data()
+	res = json.loads(data)
+	mobile = res['mobile']
+	password = res['password']
+	nick_name = res['nickName']
 	print(mobile, password, nick_name)
-	insert_user(mobile, password, nick_name)
-	
 	return jsonify({'code': 1, 'message': '注册成功', 'data': '注册成功'})
 
 
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+def allowed_file(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+	
+# 上传文件 os模块处理文件相关  os.path.join() 路径拼接
+@app.route('/upload', methods=['POST', 'GET'])
+def upload():
+	if request.method == 'POST':
+		file = request.files['file']
+		if file and allowed_file(file.filename):
+		
+			file.save(os.path.join(os.path.join(os.getcwd()), 'static', file.filename))
+			file_url = os.path.join(os.path.join(app.config['base_host']), 'static', file.filename)
+			print('file_url:', file_url);
+			return jsonify({'code': 1, 'message': '上传成功', 'data': file_url})
+	
+	
 if __name__ == "__main__":
+	app.config['JSON_AS_ASCII'] = False    # 解决返回值中文乱码
 	CORS(app, supports_credentials=True)
 	app.run()
+
+# 添加文章
+@app.route('/addArticle', methods=['POST', 'GET'])
+def add_article():
+	if request.method == 'POST':
+		data = request.get_data()
+		req = json.loads(data)
+		_id = req['id']
+		title = req['title']
+		content = req['content']
+		_userInfo = DBManager().get_user_by_id(_id)
+		print('userInfo:', _userInfo)
+		
--- models.py
+++ models.py
@@ -1,46 +1,100 @@
 import pymysql
 
-config = {
-	'host': 'localhost',
-	'user': 'root',
-	'password': 'root',
-	'port': 3306,
-	'db': 'blog'
-}
 
-connection = pymysql.connect(
-	host='localhost',
-	user='root',
-	password='root',
-	db='blog',
-	charset='utf8mb4',
-)
+# connection.cursor(pymysql.cursors.DictCursor) 增加该设置会返回字典，不设置返回元祖
 
-
-def create_user_table():
-	try:
-		with connection.cursor() as cursor:   #返回一个指针对象，用于访问和操作数据库中的数据
-			_sql = "CREATE TABLE IF NOT EXISTS `users`("\
-				   "`id` int(11) NOT NULL AUTO_INCREMENT)",\
-				   "`mobile` varchar(30) NOT NULL",\
-				   "`password` varchar(20) NOT NULL",\
-				   "`nick_name` varchar(30) NOT NULL",\
-				   "PRIMARY KEY(`id`)" \
-				   ")ENGINE=InnoDB DEFAULT=uft8 COLLATE=utf8_bin"
-			cursor.execute(_sql)
-			connection.commit()
-			
-	finally:
-		connection.close()
-
-
-def insert_user(mobile, password, nick_name):
-	try:
-		with connection.cursor() as cursor:
-			_sql = "INSERT INTO `users` (`mobile, `password`, `nick_name`) VALUES (mobile, password, nick_name)"
-			cursor.execute(_sql)
-			connection.commit()
+class DBManager(object):
+	# init 初始化会在对象创建后立即执行
+	def __init__(self):
+		self.connection = pymysql.connect(
+			host='localhost',
+			user='root',
+			password='root',
+			db='blog',
+			charset='utf8mb4',
+		)
 		
-	finally:
-		connection.close()
+	def create_user_table(self):
+		try:
+			with self.connection.cursor() as cursor:
+				_sql = "CREATE TABLE IF NOT EXISTS users(" \
+					   "id int(11) NOT NULL AUTO_INCREMENT, " \
+					   "mobile varchar(30) NOT NULL," \
+					   "password varchar(20) NOT NULL," \
+					   "nick_name varchar(30) NOT NULL,PRIMARY KEY(id)) ENGINE=InnoDB COLLATE=utf8_bin"
+				cursor.execute(_sql)
+				self.connection.commit()
 
+		finally:
+			self.connection.close()
+	
+	# 创建文章列表表
+	def create_article_list(self):
+		try:
+			with self.connection.cursor() as cursor:
+				_sql = "CREATE TABLE IF NOT EXISTS article_list(" \
+					   "id int(11) NOT NULL AUTO_INCREMENT," \
+					   "title varchar(30) NOT NULL," \
+					   "content varchar(1000) NOT NULL," \
+					   "create_time datetime NOT NULL," \
+					   "update_time datetime," \
+					   "likes int(10) NOT NULL," \
+					   "dislikes int(10) NOT NULL," \
+					   "author varchar(10) NOT NULL, " \
+					   "PRIMARY KEY(id))ENGINE=InnoDB COLLATE=utf8_bin"
+				cursor.execute(_sql)
+				self.connection.commit()
+		finally:
+			self.connection.close()
+	
+	# 添加用户
+	def insert_user(self, mobile, password, nick_name):
+		try:
+			with self.connection.cursor(pymysql.cursors.DictCursor) as cursor:
+				_sql = "INSERT INTO users (mobile, password, nick_name) VALUES (%s, %s, %s)"
+				cursor.execute(_sql, mobile, password, nick_name)
+				self.connection.commit()
+
+		finally:
+			self.connection.close()
+
+	# 通过手机号查询用户信息 fetchone() 返回一条数据， fetchall() 返回多个数据, fetchmany
+	def get_user_by_mobile(self, mobile):
+		try:
+			with self.connection.cursor(pymysql.cursors.DictCursor) as cursor:
+				_sql = "SELECT * FROM users WHERE mobile = %s"
+				cursor.execute(_sql, mobile)
+				result = cursor.fetchone()
+				print('result1:', result)
+				self.connection.commit()
+				return result
+		finally:
+			self.connection.close()
+
+	# 通过id查询用户信息
+	def get_user_by_id(self, _id):
+		try:
+			with self.connection.cursor(pymysql.cursors.DictCursor) as cursor:
+				_sql = "SELECT * FROM users WHERE id = %s"
+				cursor.execute(_sql, _id)
+				result = cursor.fetchone()
+				print('result1:', result)
+				self.connection.commit()
+				return result
+		finally:
+			self.connection.close()
+
+	# 插入文章
+	def insert_article(self,_id, _title, _content, _author, _author_id):
+		try:
+			with self.connection.cursor(pymysql.cursors.DictCursor) as cursor:
+				_sql = "INSET INTO article_list(title, content, create_time, update_time, likes, dislikes, author, author_id) VALUES(%s, %s, %s, %s, %s, %s, %s, %s)"
+				_create_time = ''
+				_update_time = ''
+				_likes = 0
+				_dislikes = 0
+				cursor.execute(_sql, _title, _content, _create_time, _update_time, _likes, _dislikes, _author, _author_id)
+				self.connection.commit()
+			
+		finally:
+			self.connection.close()
